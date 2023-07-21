14a9b306bda8a50ba87302c50792df58d18ea2ae
Eric Shaw <76802221+Esaucey@users.noreply.github.com>
2023-07-20 22:42:29 -0400

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import Logo from '../assets/Logo .png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer_container'>
+      <img src={Logo} alt="" />
+      <section className='footer_lists'>
+        <div className='footer_doormat_nav'>
+          <h4>Doormat Navigation</h4>
+          <p>Home</p>
+          <p>About</p>
+          <p>Menu</p>
+          <p>Reservation</p>
+          <p>Order Online</p>
+          <p>Login</p>
+        </div>
+        <div className='footer_contact'>
+          <h4>Contact</h4>
+          <p>Address</p>
+          <p>Phone Number</p>
+          <p>Email</p>
+        </div>
+        <div className='footer_social_links'>
+          <h4>Social Media Links</h4>
+          <p>Link 1</p>
+          <p>Link 2</p>
+          <p>Link 3</p>
+          <p>Link 4</p>
+        </div>
+      </section>
+
+    </div>
   )
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,110 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div>
+      <hero>
+        <h2>Little Lemon</h2>
+        <h3>Chicago</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse temporibus eaque maiores vero beatae tenetur libero, quisquam similique possimus necessitatibus quia nesciunt ullam exercitationem aliquid magnam ipsa minus? Aperiam, saepe.</p>
+        <button>Reserve a Table</button>
+      </hero>
+      <main>
+        <div>
+          <h2>This Weeks Specials!</h2>
+          <button>Online Menu</button>
+        </div>
+        <div className="card_container">
+          <div className="card">
+            <img src="" alt="" />
+            <div className="card_title">
+              <h4>Dish 1</h4>
+              <h4>$11.00</h4>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique quasi omnis laboriosam eum, harum autem ab delectus saepe adipisci voluptate? Odit debitis id explicabo, eius optio tempora!
+            </p>
+            <p>Order</p>
+          </div>
+          <div className="card">
+            <img src="" alt="" />
+            <div className="card_title">
+              <h4>Dish 2</h4>
+              <h4>$11.00</h4>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique quasi omnis laboriosam eum, harum autem ab delectus saepe adipisci voluptate? Odit debitis id explicabo, eius optio tempora!
+            </p>
+            <p>Order</p>
+          </div>
+          <div className="card">
+            <img src="" alt="" />
+            <div className="card_title">
+              <h4>Dish 3</h4>
+              <h4>$11.00</h4>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique quasi omnis laboriosam eum, harum autem ab delectus saepe adipisci voluptate? Odit debitis id explicabo, eius optio tempora!
+            </p>
+            <p>Order</p>
+          </div>
+          <div className="testimonials">
+            <div className="card">
+              <h4>User1</h4>
+              <div>
+                <img src="" alt="" />
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit maxime delectus laboriosam dolores earum alias cupiditate soluta pariatur quas!
+                </p>
+              </div>
+              <div className="rating"></div>
+            </div>
+            <div className="card">
+              <h4>User2</h4>
+              <div>
+                <img src="" alt="" />
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit maxime delectus laboriosam dolores earum alias cupiditate soluta pariatur quas!
+                </p>
+              </div>
+              <div className="rating"></div>
+            </div>
+            <div className="card">
+              <h4>User3</h4>
+              <div>
+                <img src="" alt="" />
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit maxime delectus laboriosam dolores earum alias cupiditate soluta pariatur quas!
+                </p>
+              </div>
+              <div className="rating"></div>
+            </div>
+            <div className="card">
+              <h4>User4</h4>
+              <div>
+                <img src="" alt="" />
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit maxime delectus laboriosam dolores earum alias cupiditate soluta pariatur quas!
+                </p>
+              </div>
+              <div className="rating"></div>
+            </div>
+            <div className="card">
+              <h4>User1</h4>
+              <div>
+                <img src="" alt="" />
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit maxime delectus laboriosam dolores earum alias cupiditate soluta pariatur quas!
+                </p>
+              </div>
+              <div className="rating"></div>
+            </div>
+          </div>
+        </div>
+        <div className='about'>
+          <section>
+            <h2>Little Lemon</h2>
+            <h3>Chicago</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum omnis illo aspernatur eaque quidem doloribus, placeat autem perferendis laborum tenetur quo asperiores aut atque recusandae neque nostrum voluptatem eius sed quisquam in! A, nobis ipsam!</p>
+          </section>
+          <section className='images'>
+            <img src="" alt="" />
+            <img src="" alt="" />
+          </section>
+        </div>
+      </main>
+    </div>
   )
 }
 
